8b419d84be485950c08045254ce68e97e43c6932
Francisco José Navarro García <fran@atalayasoft.com>
2025-01-31 19:14:47 +0100

    Initial project setup

---
+++ RickAndMorty-SwiftUI/RickAndMorty-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickAndMorty-SwiftUI
+//
+//  Created by Francisco José Navarro García on 31.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
